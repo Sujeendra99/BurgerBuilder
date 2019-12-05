8b84549e49a74195e1416fbec4d1de0f12034880
Sujeendra <sujeendrasm@gmail.com>
2019-12-05 11:50:03 +0530

    commit redux advanced

--- src/serviceWorker.js
+++ src/serviceWorker.js
@@ -41,15 +41,16 @@ export function register(config) {
         // Add some additional logging to localhost, pointing developers to the
         // service worker/PWA documentation.
         navigator.serviceWorker.ready.then(() => {
-          'This web app is being served cache-first by a service ' +
+          console.log(
+            'This web app is being served cache-first by a service ' +
             'worker. To learn more, visit https://bit.ly/CRA-PWA'
           );
-      });
-  } else {
-    // Is not localhost. Just register service worker
-    registerValidSW(swUrl, config);
-  }
-});
+        });
+      } else {
+        // Is not localhost. Just register service worker
+        registerValidSW(swUrl, config);
+      }
+    });
   }
 }
 
